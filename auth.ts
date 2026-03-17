4e7d6c24079cfe4a81ab2b068c97a9b701c6d3a6
Dima <Dima@MacBook-Air-Olha.local>
2026-03-17 09:29:47 +0200

    Credentials + SignIn page

--- auth.ts
+++ auth.ts
@@ -64,8 +64,12 @@
 // // * * * * * *
 
 
+import type {User} from 'next-auth'
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import Credentials from "next-auth/providers/credentials"
+import {users} from '@/data/users';
+
 
 const admins = [
 	"mordatiykd@gmail.com",
@@ -83,51 +87,58 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 			clientId: process.env.AUTH_GOOGLE_ID!,
 			clientSecret: process.env.AUTH_GOOGLE_SECRET!,
 		}),
+		Credentials({
+			credentials: {
+				email: { label: 'email', type: 'email', required: true },
+				password: { label: 'password', type: 'password', required: true },
+			},
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials.password) return null;
+
+				const currentUser = users.find(user => user.email === credentials.email)
+
+				if (currentUser && currentUser.password === credentials.password) {
+					const { password, ...userWithoutPass } = currentUser;
+
+					return userWithoutPass as User;
+				}
+
+				return null
+			}
+		})
 	],
 
 	callbacks: {
-
 		async signIn({ user }) {
-
 			const email = user.email
 
 			if (!email) return false
-
 			const whitelist = [...admins, ...editors]
 
 			if (!whitelist.includes(email)) {
 				return false
 			}
-
 			return true
 		},
 
 		async jwt({ token }) {
-
 			if (token.email) {
-
 				if (admins.includes(token.email)) {
 					token.role_id = 1
 				}
-
 				if (editors.includes(token.email)) {
 					token.role_id = 2
 				}
-
 			}
-
 			return token
 		},
 
 		async session({ session, token }) {
-
 			if (session.user) {
 				session.user.role_id = token.role_id as number
 			}
-
 			return session
 		},
-
 	},
 
 })
